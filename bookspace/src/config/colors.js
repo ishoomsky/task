9d85d2b057342a190f20d40ceffc1f90023da2ae
Ivan Shumski <ishoomsky@gmail.com>
2021-08-16 16:48:26 +0300

    add theme colors

---
+++ bookspace/src/config/colors.js
@@ -0,0 +1,13 @@
+const colors = {
+  white: "rgba(255, 255, 255, 1)",
+  gray: "rgba(18, 18, 18, 0.68)",
+  grayExtraLight: "rgba(249, 249, 249, 1)",
+  grayLight: "rgba(196, 196, 196, 1)",
+  grayDark: "rgba(18, 18, 18, 0.89)",
+  orange: "rgba(239, 63, 10, 1)",
+  body: {
+    grayExtraLight: "rgba(240, 240, 240, 1)",
+  },
+};
+
+export default colors;
