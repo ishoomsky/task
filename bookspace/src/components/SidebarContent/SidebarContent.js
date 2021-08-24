ef81ca5f50e430cf29a79a95ea290b754386d87c
Ivan Shumski <ishoomsky@gmail.com>
2021-08-24 17:42:49 +0300

    improve books sorting

--- bookspace/src/components/SidebarContent/SidebarContent.js
+++ bookspace/src/components/SidebarContent/SidebarContent.js
@@ -9,9 +9,13 @@ const SidebarContent = () => {
   const [books] = useState(initData);
   const [news] = useState(newsInit);
 
+  const rateBooks = books
+    .filter((book) => book.rating >= 4)
+    .sort((a, b) => b.rating - a.rating);
+
   return (
     <>
-      <RatedBooks books={books} />
+      <RatedBooks books={rateBooks} />
       <News news={news} />
     </>
   );
